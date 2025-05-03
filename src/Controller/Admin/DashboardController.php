87a828bf58b4c64362fa5a7a0ad17dd07b557149
slinkybass <israel@garaballu.com>
2025-05-04 01:54:11 +0200

    Customized user menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -7,10 +7,12 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Locale;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Intl\Languages;
+use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
@@ -69,4 +71,25 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
     }
+
+	public function configureUserMenu(UserInterface $userInterface): UserMenu
+	{
+        /** @var User $user */
+        $user = $userInterface;
+		$userMenu = parent::configureUserMenu($userInterface);
+
+		if ($user->getAvatar()) {
+			$userMenu->setAvatarUrl($user->getAvatar());
+		}
+
+		$menuItems = array();
+		if ($this->isGranted('IS_IMPERSONATOR')) {
+			$menuItems[] = MenuItem::linkToExitImpersonation($this->translator->trans('user.exit_impersonation', [], 'EasyAdminBundle'), 'icon ti ti-user-x');
+		} else {
+			$menuItems[] = MenuItem::linkToLogout($this->translator->trans('user.sign_out', [], 'EasyAdminBundle'), 'icon ti ti-logout');
+		}
+		$userMenu->setMenuItems($menuItems);
+
+		return $userMenu;
+	}
 }
