e20e0be73ebb0ec99b222d93e49c9869213b7ed7
slinkybass <israel@garaballu.com>
2025-05-03 02:05:49 +0200

    Created admin_home path

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -6,21 +6,30 @@ use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\HttpFoundation\Response;
 
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
+    private AdminUrlGenerator $adminUrlGenerator;
+
+    public function __construct(AdminUrlGenerator $adminUrlGenerator)
+    {
+        $this->adminUrlGenerator = $adminUrlGenerator;
+    }
+
     public function index(): Response
     {
-        return parent::index();
+        $url = $this->adminUrlGenerator->setRoute('admin_home')->generateUrl();
+        return $this->redirect($url);
     }
 
     public function configureDashboard(): Dashboard
     {
 		$dashboard = Dashboard::new();
 
-        $dashboard->setTitle('Symfonybase');
+        $dashboard->setTitle('SymfonyBase');
 
         return $dashboard;
     }
---
+++ src/Controller/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class AdminController extends AbstractController
+{
+    #[Route('/admin/home', name: 'admin_home')]
+    public function index(): Response
+    {
+        return $this->render('admin/home.html.twig');
+    }
+}
