b8bbe9efaea96d38b81b8c9d0ceef90b16790fe9
slinkybass <israel@garaballu.com>
2025-05-19 00:30:23 +0200

    Delete and batch actions

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -2,6 +2,8 @@ import Admin from "./admin.controller.js";
 
 document.addEventListener("DOMContentLoaded", function () {
 	Admin.createFilters();
+	Admin.createBatchActions();
+	Admin.createModalWindowsForDeleteActions();
 });
 
 // CSS
