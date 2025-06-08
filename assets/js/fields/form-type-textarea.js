736d2f99d74732afe168bbb2208bfa5807ccc601
slinkybass <israel@garaballu.com>
2025-06-08 05:38:40 +0200

    Format form type textarea js

--- assets/js/fields/form-type-textarea.js
+++ assets/js/fields/form-type-textarea.js
@@ -5,38 +5,40 @@
  * Version: 3.0
  */
 
-document.addEventListener("DOMContentLoaded", () => {
-	formTypeTextarea();
-});
-document.addEventListener("ea.collection.item-added", () => {
-	formTypeTextarea();
-});
-
-window.formTypeTextarea = function formTypeTextarea(selector = '[data-textarea-field="true"]') {
-	document.querySelectorAll(selector).forEach((e) => {
-		e.addEventListener("input", function () {
-			autogrow(this);
-		});
-		autogrow(e);
+(function () {
+	document.addEventListener("DOMContentLoaded", () => {
+		formTypeTextarea();
+	});
+	document.addEventListener("ea.collection.item-added", () => {
+		formTypeTextarea();
 	});
 
-	function autogrow(field) {
-		const maxHeight = field.hasAttribute("data-textarea-max-height") ? field.getAttribute("data-textarea-max-height") : "200px";
+	window.formTypeTextarea = function formTypeTextarea(selector = '[data-textarea-field="true"]') {
+		document.querySelectorAll(selector).forEach((e) => {
+			e.addEventListener("input", function () {
+				autogrow(this);
+			});
+			autogrow(e);
+		});
 
-		field.style.overflow = "hidden";
-		field.style.resize = "none";
-		field.style.boxSizing = "border-box";
-		field.style.height = "auto";
-		field.style.maxHeight = maxHeight;
+		function autogrow(field) {
+			const maxHeight = field.hasAttribute("data-textarea-max-height") ? field.getAttribute("data-textarea-max-height") : "200px";
 
-		// this check is needed because the <textarea> element can be inside a
-		// minimizable panel, causing its scrollHeight value to be 0
-		if (field.scrollHeight > 0) {
-			field.style.height = field.scrollHeight + "px";
+			field.style.overflow = "hidden";
+			field.style.resize = "none";
+			field.style.boxSizing = "border-box";
+			field.style.height = "auto";
+			field.style.maxHeight = maxHeight;
+
+			// this check is needed because the <textarea> element can be inside a
+			// minimizable panel, causing its scrollHeight value to be 0
+			if (field.scrollHeight > 0) {
+				field.style.height = field.scrollHeight + "px";
+			}
+
+			if (parseInt(field.style.height.replace("px", "")) > parseInt(maxHeight.replace("px", ""))) {
+				field.style.overflow = "auto";
+			}
 		}
-
-		if (parseInt(field.style.height.replace("px", "")) > parseInt(maxHeight.replace("px", ""))) {
-			field.style.overflow = "auto";
-		}
-	}
-};
+	};
+})();
