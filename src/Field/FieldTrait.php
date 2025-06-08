da4e98535832382afd746ea2f2f00542cd643986
slinkybass <israel@garaballu.com>
2025-06-08 03:02:06 +0200

    Created FieldTrait

---
+++ src/Field/FieldTrait.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Field;
+
+trait FieldTrait
+{
+    public const OPTION_MAPPED = 'mapped';
+    public const OPTION_REQUIRED = 'required';
+    public const OPTION_DATA = 'data';
+    public const OPTION_PLACEHOLDER = 'placeholder';
+    public const OPTION_MAX_LENGTH = 'maxlength';
+    public const OPTION_MIN_LENGTH = 'minlength';
+    public const OPTION_RENDER_AS_HTML = 'renderAsHtml';
+    public const OPTION_STRIP_TAGS = 'stripTags';
+
+    public function __call(string $method, array $args)
+    {
+        $result = $this->field->$method(...$args);
+
+        if ($result === $this->field) {
+            return $this;
+        }
+
+        return $result;
+    }
+
+    public function getField(): object
+    {
+        return $this->field;
+    }
+
+    public function setMapped(bool $val = true): self
+    {
+        $this->field->setFormTypeOption(self::OPTION_MAPPED, $val);
+
+        return $this;
+    }
+
+    public function setRequired(bool $val = true): self
+    {
+        $this->field->setFormTypeOption(self::OPTION_REQUIRED, $val);
+
+        return $this;
+    }
+
+    public function setData(mixed $val): self
+    {
+        $this->field->setFormTypeOption(self::OPTION_DATA, $val);
+
+        return $this;
+    }
+
+    public function setPlaceholder(?string $val): self
+    {
+        $this->field->setHtmlAttribute(self::OPTION_PLACEHOLDER, $val);
+
+        return $this;
+    }
+
+    public function setMaxLength(?int $val): self
+    {
+        $this->field->setHtmlAttribute(self::OPTION_MAX_LENGTH, $val);
+
+        return $this;
+    }
+
+    public function setMinLength(?int $val): self
+    {
+        $this->field->setHtmlAttribute(self::OPTION_MIN_LENGTH, $val);
+
+        return $this;
+    }
+
+    public function renderAsHtml(bool $val = true): self
+    {
+        $this->field->setCustomOption(self::OPTION_RENDER_AS_HTML, $val);
+
+        return $this;
+    }
+
+    public function stripTags(bool $val = true): self
+    {
+        $this->field->setCustomOption(self::OPTION_STRIP_TAGS, $val);
+
+        return $this;
+    }
+}
