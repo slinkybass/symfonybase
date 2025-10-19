ac42c185faaa69ecc00b5bdbab52022f06358893
slinkybass <israel@garaballu.com>
2025-10-19 06:21:26 +0200

    Format code

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -14,9 +14,9 @@ final class AdminController extends AbstractController
         return $this->render('admin/home.html.twig');
     }
 
-	#[Route('/admin/media', name: 'admin_media')]
-	public function admin_media(): Response
-	{
-		return $this->render('admin/media.html.twig');
-	}
+    #[Route('/admin/media', name: 'admin_media')]
+    public function admin_media(): Response
+    {
+        return $this->render('admin/media.html.twig');
+    }
 }
