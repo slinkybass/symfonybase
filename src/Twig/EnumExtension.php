b6aaf844d4178a206e7bd9d50939cd7477ae90f2
slinkybass <israel@garaballu.com>
2026-04-05 06:07:16 +0200

    Upgraded EnumExtension

--- src/Twig/EnumExtension.php
+++ src/Twig/EnumExtension.php
@@ -6,9 +6,10 @@ use Symfony\Contracts\Translation\TranslatableInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
+use Twig\TwigFunction;
 
 /**
- * Twig extension to display and resolve PHP enums in Twig templates.
+ * Twig extension to display, resolve and introspect PHP enums in Twig templates.
  */
 class EnumExtension extends AbstractExtension
 {
@@ -19,11 +20,38 @@ class EnumExtension extends AbstractExtension
     public function getFilters(): array
     {
         return [
+            new TwigFilter('enum_name', [$this, 'enumName']),
+            new TwigFilter('enum_value', [$this, 'enumValue']),
             new TwigFilter('enum_label', [$this, 'enumLabel']),
             new TwigFilter('enum_choices', [$this, 'enumChoices']),
             new TwigFilter('enum_from_value', [$this, 'enumFromValue']),
             new TwigFilter('enum_from_name', [$this, 'enumFromName']),
+            new TwigFilter('enum_is', [$this, 'enumIs']),
         ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('enum_cases', [$this, 'enumCases']),
+            new TwigFunction('enum_count', [$this, 'enumCount']),
+        ];
+    }
+
+    /**
+     * Returns the name of an enum case.
+     */
+    public function enumName(\UnitEnum $enum): string
+    {
+        return $enum->name;
+    }
+
+    /**
+     * Returns the backing value of a backed enum case, or null for pure enums.
+     */
+    public function enumValue(\UnitEnum $enum): int|string|null
+    {
+        return $enum instanceof \BackedEnum ? $enum->value : null;
     }
 
     /**
@@ -42,7 +70,7 @@ class EnumExtension extends AbstractExtension
     }
 
     /**
-     * Returns all cases of an enum class as a label-value array.
+     * Returns all cases of an enum class as a label-value array, suitable for form choices.
      *
      * @param class-string $enumClass the fully qualified enum class name
      * @param string|null  $locale    the locale code (defaults to the current locale)
@@ -54,6 +82,7 @@ class EnumExtension extends AbstractExtension
         $choices = [];
         foreach ($enumClass::cases() as $case) {
             $label = $case instanceof TranslatableInterface ? $case->trans($this->translator, $locale) : $case->name;
+
             $choices[$label] = $case->value;
         }
 
@@ -68,13 +97,7 @@ class EnumExtension extends AbstractExtension
      */
     public function enumFromValue(mixed $value, string $enumClass): ?\UnitEnum
     {
-        foreach ($enumClass::cases() as $case) {
-            if ($case->value === $value) {
-                return $case;
-            }
-        }
-
-        return null;
+        return $enumClass::tryFrom($value);
     }
 
     /**
@@ -85,12 +108,52 @@ class EnumExtension extends AbstractExtension
      */
     public function enumFromName(string $name, string $enumClass): ?\UnitEnum
     {
-        foreach ($enumClass::cases() as $case) {
-            if ($case->name === $name) {
-                return $case;
-            }
+        $cases = array_combine(
+            array_map(fn (\UnitEnum $c) => $c->name, $enumClass::cases()),
+            $enumClass::cases(),
+        );
+
+        return $cases[$name] ?? null;
+    }
+
+    /**
+     * Returns whether the given enum case matches another case, a name or a value.
+     *
+     * @param \UnitEnum                 $enum  the enum case to compare
+     * @param \UnitEnum|int|string|null $other the case, name or value to compare against
+     */
+    public function enumIs(\UnitEnum $enum, \UnitEnum|int|string|null $other): bool
+    {
+        if ($other instanceof \UnitEnum) {
+            return $enum === $other;
         }
 
-        return null;
+        if ($enum instanceof \BackedEnum && $enum->value === $other) {
+            return true;
+        }
+
+        return $enum->name === $other;
+    }
+
+    /**
+     * Returns all cases of an enum class as an array of enum instances.
+     *
+     * @param class-string $enumClass the fully qualified enum class name
+     *
+     * @return \UnitEnum[]
+     */
+    public function enumCases(string $enumClass): array
+    {
+        return $enumClass::cases();
+    }
+
+    /**
+     * Returns the number of cases in an enum class.
+     *
+     * @param class-string $enumClass the fully qualified enum class name
+     */
+    public function enumCount(string $enumClass): int
+    {
+        return count($enumClass::cases());
     }
 }
