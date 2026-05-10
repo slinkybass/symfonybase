7294e5d1b1e1845b2ef4c07077d304ae973a8f49
slinkybass <israel@garaballu.com>
2026-05-11 00:59:44 +0200

    fix(User): return empty array in getRoles when role is null

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -153,6 +153,10 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     public function getRoles(): array
     {
+        if ($this->role === null) {
+            return [];
+        }
+
         $roles = [$this->role->getName()];
         if ($this->role->isAdmin()) {
             $roles[] = 'ROLE_ADMIN';
